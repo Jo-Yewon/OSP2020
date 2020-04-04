8f3e9f25429a7d7f6b4774172efe9fe14d699b9e
Jo-Yewon <yeawonjo@gmail.com>
2020-04-05 01:35:00 +0900

    Initial Commit

---
+++ OSP2020/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OSP2020
+//
+//  Created by Yewon on 2020/04/05.
+//  Copyright © 2020 Yewon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
